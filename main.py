ca273e56d3f9c1231cdc914dd9b00985d1054ff5
Talkzy <keremalpdemir@icloud.com>
2025-07-24 21:55:50 +0200

    Added a new file called systemprompt.txt allowing for easier use of longer prompts, feel free to change the example prompt I added in :)

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import re
 
 client = Client(host='http://localhost:11434')
 
+with open('systemprompt.txt', 'r') as prompt_file:
+    system_prompt = prompt_file.read()
+
 text_to_summarize = input("Enter the text/meeting notes to summarize: ")
 
 def clean_output(text):
@@ -18,7 +21,7 @@ def write_log(message):
 summary = client.chat(model='deepseek-r1', messages=[
 {
     'role': 'system',
-    'content': 'You are a professional summarizer, you will be given texts and meetings notes to summarize using the following format: 1. Subject- what the notes/text is about, around 1 sentence, 2. Main topics- What the text is actually about, more in depth and including explanations, 3. Key points- include stuff that should be remembered, 4. Anything else to add- such as important dates or events, 5. A paragraph length conclusion which explains the text/meeting.',
+    'content': system_prompt,
 },
 {
     'role': 'user',
